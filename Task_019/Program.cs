76af6e74a900d17d93fde4fe4bf01cd327754503
Kulikov Alexander <shouter84@yandex.ru>
2023-04-30 23:51:54 +0300

    Added solution of Task_019

--- Task_019/Program.cs
+++ Task_019/Program.cs
@@ -6,3 +6,21 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+System.Console.WriteLine("Enter five-digit number, PLEASE");
+string? num = Console.ReadLine();
+System.Console.WriteLine("Is this number a palindrome?");
+if (num!.Length < 5 || num!.Length > 5) 
+{
+    System.Console.WriteLine("Number you have just entered is NOT a five-digit");
+}
+else if (num![0] == num![4] && num![1] == num![3])
+{
+    System.Console.WriteLine("YES, this number is a palindrome!");
+}
+else 
+{
+    System.Console.WriteLine("NO, it's not");
+}
+
+
