2295909071a784f308c958a0702c29e59ab781dc
Kulikov Alexander <shouter84@yandex.ru>
2023-04-30 22:30:59 +0300

    Added solution of Task_021 and Task_023

---
+++ Task_021/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 21:
+
+// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] coordsA = new int[3];
+coordsA[0] = ReadInt("Введите координату Х точки А");
+coordsA[1] = ReadInt("Введите координату Y точки А");
+coordsA[2] = ReadInt("Введите координату Z точки А");
+
+int[] coordsB = new int[3];
+coordsB[0] = ReadInt("Введите координату Х точки B");
+coordsB[1] = ReadInt("Введите координату Y точки B");
+coordsB[2] = ReadInt("Введите координату Z точки B");
+
+double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) + Math.Pow(coordsA[1] - coordsB[1], 2) + Math.Pow(coordsA[2] - coordsB[2], 2));
+
+System.Console.WriteLine(Math.Round(distance, 2));
+
+// int[] coordsA = new int[3];
+
+// System.Console.WriteLine("Введите координату Х точки А");
+// coordsA[0] = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Y точки А");
+// coordsA[1] = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Z точки А");
+// coordsA[2] = Convert.ToInt32(Console.ReadLine());
+
+// int[] coordsB = new int[3];
+
+// System.Console.WriteLine("Введите координату Х точки B");
+// coordsB[0] = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Y точки B");
+// coordsB[1] = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Z точки B");
+// coordsB[2] = Convert.ToInt32(Console.ReadLine());
+
+// double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) + Math.Pow(coordsA[1] - coordsB[1], 2) + Math.Pow(coordsA[2] - coordsB[2], 2));
+
+// System.Console.WriteLine(Math.Round(distance, 2));
---
+++ Task_023/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23:
+
+// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = Math.Abs(ReadInt("Введите целое число: "));
+// for (int i = 1; i <= num; i++)
+// {
+//     System.Console.WriteLine($" {Math.Pow(i, 3)}");
+// }
+int i = 1;
+while (i <= num)
+{
+    System.Console.Write(" " + Math.Pow(i, 3));
+    i++;  
+}
